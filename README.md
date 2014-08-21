ea80eb87d63bffea59bf4b1dff35ac0fa61b2648
Didip Kerabat <didipk@gmail.com>
2014-08-20 22:59:22 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+## URL Shortener Service
+
+This simple service Base36 encodes URL and store them in file system.
+
+It has 3 features: shorten, unshorten, and redirect.
+
+
+### Dependency
+
+`github.com/gorilla/mux`
+
+### Why?
+
+By itself, URL shortening is quite useful.
+
+But this project exists to demonstrate:
+
+* How short [Go](http://golang.org/) & to the point is: 80 lines.
+
+* How slim Go is: 3MB RAM.
+
+* How performant Go is:
+    ```
+    # Command  : ab -n 100000 -c 200 -k http://localhost:8080/dec/4
+    # Processor: 2.26 GHz Intel Core 2 Duo
+
+    Concurrency Level:      200
+    Time taken for tests:   9.678 seconds
+    Complete requests:      100000
+    Failed requests:        0
+    Write errors:           0
+    Keep-Alive requests:    100000
+    Total transferred:      16000000 bytes
+    HTML transferred:       1900000 bytes
+    Requests per second:    10333.08 [#/sec] (mean)
+    Time per request:       19.355 [ms] (mean)
+    Time per request:       0.097 [ms] (mean, across all concurrent requests)
+    Transfer rate:          1614.54 [Kbytes/sec] received
+    ```
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/user"
+	"strconv"
+)
+
+type Base36Url struct {
+	Root string
+}
+
+func (s *Base36Url) Init(root string) {
+	s.Root = root
+	os.MkdirAll(s.Root, 0744)
+}
+
+func (s *Base36Url) Path(code string) string {
+	return s.Root + code
+}
+
+func (s *Base36Url) Save(url string) string {
+	files, _ := ioutil.ReadDir(s.Path(""))
+	numFiles := len(files)
+	code := strconv.FormatUint(uint64(numFiles+1), 36)
+
+	ioutil.WriteFile(s.Path(code), []byte(url), 0744)
+	return code
+}
+
+func (s *Base36Url) EncodeHandler(w http.ResponseWriter, r *http.Request) {
+	url := r.FormValue("url")
+	if url != "" {
+		fmt.Fprintf(w, s.Save(url))
+	}
+}
+
+func (s *Base36Url) DecodeHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	code := params["code"]
+	url, err := ioutil.ReadFile(s.Path(code))
+
+	if err == nil {
+		fmt.Fprintf(w, string(url))
+	} else {
+		fmt.Fprintf(w, "Error: %v", err)
+	}
+}
+
+func (s *Base36Url) RedirectHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	code := params["code"]
+	url, err := ioutil.ReadFile(s.Path(code))
+
+	if err == nil {
+		http.Redirect(w, r, string(url), 301)
+	}
+}
+
+func main() {
+	usr, _ := user.Current()
+	storage := &Base36Url{}
+	storage.Init(usr.HomeDir + "/shawty/")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", storage.EncodeHandler).Methods("POST")
+	r.HandleFunc("/dec/{code}", storage.DecodeHandler).Methods("GET")
+	r.HandleFunc("/red/{code}", storage.RedirectHandler).Methods("GET")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.Handle("/", r)
+	http.ListenAndServe(":"+port, nil)
+}
