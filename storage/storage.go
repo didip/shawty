af5766daf0131d632ed1a143ff7660a9c80c78a6
Thomas Desrosiers <thomas@databricks.com>
2016-02-13 15:45:16 -0800

    Storge naming doesn't have to be deterministic

--- storage/storage.go
+++ storage/storage.go
@@ -2,8 +2,6 @@
 package storages
 
 type Storage interface {
-	// Hash(string) returns the short URL that would be generated from a long URL
-	Code(string) string
 	// Save(string) takes a full URL and returns the short URL after saving it to storage
 	Save(string) (string, error)
 	// Load(string) takes a short URL and returns the original full URL by retrieving it from storage
