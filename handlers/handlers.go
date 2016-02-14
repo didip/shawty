3d1b95f491c8aab501e3412259ea0522ff778ac6
Thomas Desrosiers <thomas@databricks.com>
2016-02-13 18:14:59 -0800

    Add Redirect support

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,56 +2,61 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/thomaso-mirodin/shawty/storages"
+	"github.com/julienschmidt/httprouter"
+	"github.com/thomaso-mirodin/shawty/storage"
 )
 
-func EncodeHandler(storage storages.Storage) http.Handler {
-	handleFunc := func(w http.ResponseWriter, r *http.Request) {
-		if url := r.PostFormValue("url"); url != "" {
-			code, err := storage.Save(url)
-			if err != nil {
-				http.Error(w, err, 500)
-				return
-			}
-			w.Write([]byte(code))
-		}
-	}
-
-	return http.HandlerFunc(handleFunc)
+func Index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	fmt.Fprintln(w, "Hello world!")
 }
 
-func DecodeHandler(storage storages.Storage) http.Handler {
-	handleFunc := func(w http.ResponseWriter, r *http.Request) {
-		code := r.URL.Path[len("/dec/"):]
+func RedirectHandler(storage storage.Storage) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		code := ps.ByName("short")
 
 		url, err := storage.Load(code)
 		if err != nil {
 			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("URL Not Found. Error: " + err.Error() + "\n"))
+			fmt.Fprintln(w, "URL Not Found.")
 			return
 		}
 
-		w.Write([]byte(url))
+		http.Redirect(w, r, string(url), http.StatusFound)
 	}
-
-	return http.HandlerFunc(handleFunc)
 }
 
-func RedirectHandler(storage storages.Storage) http.Handler {
-	handleFunc := func(w http.ResponseWriter, r *http.Request) {
-		code := r.URL.Path[len("/red/"):]
+// func UnknownRedirectHandler(storage storages.Unnamed) httprouter.Handler {
+// 	handleFunc := func(w http.ResponseWriter, r *http.Request) {
+// 		if url := r.PostFormValue("url"); url != "" {
+// 			code, err := storage.Save(url)
+// 			if err != nil {
+// 				http.Error(w, err, 500)
+// 				return
+// 			}
+// 			w.Write([]byte(code))
+// 		}
+// 	}
+// }
 
-		url, err := storage.Load(code)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("URL Not Found. Error: " + err.Error() + "\n"))
-			return
-		}
+// 	return http.HandlerFunc(handleFunc)
+// }
 
-		http.Redirect(w, r, string(url), 301)
-	}
+// func DecodeHandler(storage storages.Storage) http.Handler {
+// 	handleFunc := func(w http.ResponseWriter, r *http.Request) {
+// 		code := r.URL.Path[len("/dec/"):]
 
-	return http.HandlerFunc(handleFunc)
-}
+// 		url, err := storage.Load(code)
+// 		if err != nil {
+// 			w.WriteHeader(http.StatusNotFound)
+// 			w.Write([]byte("URL Not Found. Error: " + err.Error() + "\n"))
+// 			return
+// 		}
+
+// 		w.Write([]byte(url))
+// 	}
+
+// 	return http.HandlerFunc(handleFunc)
+// }
--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/thomaso-mirodin/shawty/handlers"
-	"github.com/thomaso-mirodin/shawty/storages"
+	"github.com/thomaso-mirodin/shawty/storage"
 )
 
 func main() {
@@ -16,21 +17,21 @@ func main() {
 		log.Fatal(err)
 	}
 
-	path := filepath.Join(dir, "shawty")
-	storage, err := storages.NewFilesystem(path)
+	path := filepath.Join(dir, "shawty", "filesystem_db")
+	store, err := storage.NewFilesystem(path)
 	if err != nil {
 		log.Fatalf("Failed to create filesystem '%s' because '%s'", path, err)
 	}
 
-	http.Handle("/", handlers.EncodeHandler(storage))
-	http.Handle("/dec/", handlers.DecodeHandler(storage))
-	http.Handle("/red/", handlers.RedirectHandler(storage))
+	r := httprouter.New()
+	r.GET("/", handlers.Index)
+	r.GET("/:short", handlers.RedirectHandler(store))
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	err = http.ListenAndServe(":"+port, nil)
+	err = http.ListenAndServe(":"+port, r)
 	if err != nil {
 		log.Fatal(err)
 	}
