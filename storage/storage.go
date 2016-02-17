de77e3f70474064d56455daf63c4d8b62101f85e
Thomas Desrosiers <thomas@databricks.com>
2016-02-16 19:18:44 -0800

    Add some new errors

--- storage/storage.go
+++ storage/storage.go
@@ -1,6 +1,8 @@
 // Package storages allows multiple implementation on how to store short URLs.
 package storage
 
+import "errors"
+
 type Storage interface {
 	// Load(string) takes a short URL and returns the original full URL by retrieving it from storage
 	Load(string) (string, error)
@@ -17,3 +19,6 @@ type NamedStorage interface {
 	// SaveName takes a name and a url and saves the name to use for saving a url
 	SaveName(name string, url string) error
 }
+
+var ErrCodeInUse = errors.New("tried to set short, but unable to find a unique shortname")
+var ErrCodeNotSet = errors.New("storage layer doens't have a url for that short code")
