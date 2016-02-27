bc65024f159304d5de2a867aa02844632f23c88c
Thomas Desrosiers <thomas@databricks.com>
2016-02-27 02:22:19 -0800

    S3 Refactor

--- storage/s3.go
+++ storage/s3.go
@@ -1,17 +1,28 @@
 package storage
 
 import (
+	"crypto/sha512"
+	"encoding/json"
+	"fmt"
+	"path"
+	"time"
+
 	"github.com/mitchellh/goamz/aws"
 	"github.com/mitchellh/goamz/s3"
 )
 
 type S3 struct {
-	Bucket *s3.Bucket
+	Bucket   *s3.Bucket
+	hashFunc func(string) string
 }
 
 func NewS3(auth aws.Auth, region aws.Region, bucketName string) (*S3, error) {
 	s := &S3{
 		Bucket: s3.New(auth, region).Bucket(bucketName),
+		hashFunc: func(s string) string {
+			h := sha512.Sum384([]byte(s))
+			return string(h[:])
+		},
 	}
 
 	_, err := s.Bucket.GetBucketContents()
@@ -20,6 +31,45 @@ func NewS3(auth aws.Auth, region aws.Region, bucketName string) (*S3, error) {
 	}
 
 	return s, err
+}
+
+func (s *S3) saveKey(short, url string) (err error) {
+	hashedShort := s.hashFunc(short)
+
+	err = s.Bucket.Put(
+		path.Join(hashedShort, "long"),
+		[]byte(url),
+		"text/plain",
+		s3.BucketOwnerFull,
+	)
+	if err != nil {
+		return err
+	}
+
+	changeLog, err := json.Marshal(
+		struct {
+			URL  string
+			User string
+		}{
+			url,
+			"TODO",
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("unable to format change history: %v", err)
+	}
+
+	err = s.Bucket.Put(
+		path.Join(hashedShort, "change_history", time.Now().Format(time.RFC3339Nano)),
+		changeLog,
+		"application/json",
+		s3.BucketOwnerFull,
+	)
+	if err != nil {
+		return fmt.Errorf("unable to save change history: %v", err)
+	}
+
+	return nil
 }
 
 func (s *S3) Save(url string) (string, error) {
@@ -36,10 +86,7 @@ func (s *S3) Save(url string) (string, error) {
 		short = getRandomString(8)
 
 		if _, err = s.Bucket.GetKey(short); err != nil {
-			err = s.Bucket.Put(short, []byte(url), "text/plain", s3.BucketOwnerFull)
-			if err == nil {
-				return short, nil
-			}
+			return short, s.saveKey(short, url)
 		}
 	}
 
@@ -54,7 +101,7 @@ func (s *S3) SaveName(short string, url string) error {
 		return err
 	}
 
-	return s.Bucket.Put(short, []byte(url), "text/plain", s3.BucketOwnerFull)
+	return s.saveKey(short, url)
 }
 
 func (s *S3) Load(short string) (string, error) {
@@ -62,7 +109,7 @@ func (s *S3) Load(short string) (string, error) {
 		return "", err
 	}
 
-	url, err := s.Bucket.Get(short)
+	url, err := s.Bucket.Get(path.Join(s.hashFunc(short), "long"))
 	if s3err, ok := err.(*s3.Error); ok && s3err.Code == "NoSuchKey" {
 		return "", ErrShortNotSet
 	}
